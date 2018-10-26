5728cb1eb0d3c073a06a67694736ae1a1d96a8a7
serg10diaz <44394698+serg10diaz@users.noreply.github.com>
2018-10-26 09:27:59 +0200

    Update calendario.pl

--- calendario.pl
+++ calendario.pl
@@ -11,7 +11,7 @@ if($ENV{'QUERY_STRING'} eq "") {
   while(<F>) {
     chomp;
      @mes=split(":");
-     $ar{$mes[0]}=$prefijo[1];
+     $ar{$mes[0]}=$mes[1];
   }
   @arGet=split("=",$ENV{'QUERY_STRING'});
   print "El mes seleccionado es; $ar{$arGet[1]}<br>";
